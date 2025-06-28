dfa662ab2e4d6413a91d75604d294f9443a5479c
Betsegaw Abebe <betsegawa241@gmail.com>
2025-06-28 11:14:06 +0300

    applications component types added

--- backend/models/applications.js
+++ backend/models/applications.js
@@ -2,11 +2,26 @@ const mongoose = require('mongoose');
 
 const applicationSchema = new mongoose.Schema(
   {
-    companyName: { type: String, required: true },
-    jobType: { type: String, required: true },
-    jobStyle: { type: String, required: true },
-    role: { type: String, required: true },
-    location: { type: String, required: true },
+    company: { type: String, required: true },
+    jobTitle: { type: String, required: true },
+    jobType: {
+      type: String,
+      enum: ['Full Time', 'Part Time', 'Contract'],
+      required: true,
+    },
+    workLocation: {
+      type: String,
+      enum: ['Onsite', 'Remote', 'Hybrid'],
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['Applied', 'Interview', 'Offer', 'Rejected'],
+      default: 'Applied',
+    },
+    date: { type: String, required: true },
+    notes: { type: String },
+    interviewDateTime: { type: String },
   },
   { timestamps: true }
 );
