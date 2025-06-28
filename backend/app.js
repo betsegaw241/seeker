6d594fe940f3666967bedfecf0ba7a62f73306cf
Betsegaw Abebe <betsegawa241@gmail.com>
2025-06-28 11:12:37 +0300

    application types added

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const userRoutes = require('./routes/userRoutes');
 const applicationRoutes = require('./routes/applicationRoutes');
 const cors = require('cors');
-const authRoutes = require('./routes/auth');
+// const authRoutes = require('./config/');
 
 const app = express();
 app.use(cors());
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/api/auth', authRoutes);
+// app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/applications', applicationRoutes);
 
