47a4c88cc7d4a43e207b68cbc66a60f52edcef02
Betsegaw Abebe <betsegawa241@gmail.com>
2025-06-30 11:11:28 +0300

    minor changes in application  types

--- fontend/src/pages/application/types.ts
+++ fontend/src/pages/application/types.ts
@@ -1,14 +1,12 @@
-// hooks/applications.ts
 
 export interface JobApplication {
-    _id?: string; // optional if created by MongoDB
-    company: string;
-    jobTitle: string;
-    jobType?: string;
-    workLocation?: string;
-    notes?: string;
-    status?: string;
-    date?: string;
-    interviewDateTime?: string;
-  }
-  
+  _id?: string;
+  company: string;
+  jobTitle: string;
+  jobType: string;
+  workLocation: string;
+  notes?: string;
+  status: string;
+  date: string;
+  interviewDateTime?: string;
+}
